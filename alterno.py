88327869b3e5ef134c03dfe4b5866660a58b747f
Javier Naranjo <javier_sbt95@hotmail.com>
2023-02-18 20:43:24 -0500

    tts added
    loquendo tts is necessary

--- alterno.py
+++ alterno.py
@@ -1,18 +1,35 @@
 import keyboard
-from playsound import playsound
 import os
 import time
 import configparser
+import pygame
 
+pygame.mixer.init()
 config = configparser.ConfigParser()
 config.read('config.ini')
 insertada = False
+restante = 0
 unaVez = True
 bloqueado = False
 time_start = 0
 cont_monedas = 0
 minutos_moneda = int(config['RETROANTENA']['minutos'])
-teclas = {"a", "s", "d", "z", "x", "c", "u", "i", "o", "j", "k", "l"}
+teclas = {"a", "s", "d", "z", "x", "c", "u", "i", "o", "j", "k", "l", "enter"}
+
+###################################################################
+def Hablar(txt):
+    try:
+        pygame.mixer.music.unload()
+        if os.path.exists("audio001.wav"):
+            os.remove("audio001.wav")
+        f = open("texto.txt","w+")
+        f.write(txt)
+        f.close()
+        os.system('ejecutar.cmd')
+        pygame.mixer.music.load("audio001.wav")
+        pygame.mixer.music.play()
+    except Exception as e:
+        print(e)
 
 ###################################################################
 def Cerrar():
@@ -45,34 +62,35 @@ def Hide():
     time_start = time.time()
     cont_monedas = cont_monedas + 1
     Desbloquear()
+    falta = int((((time_start + (cont_monedas * minutos_moneda * 60)) - time.time()) + 10) / 60)
+    Hablar("Te quedan " + str(falta) + " minutos   p")
+    print("Te quedan " + str(falta) + " minutos")
 
 ###################################################################
 def InsertaMoneda():
     try:
-        print(os.path.join(os.getcwd(), "moneda.wav"))
-        playsound(os.path.join(os.getcwd(), "moneda.wav"))
+        Hablar("Inserta una moneda   p")
     except Exception as e:
         print("problema Sonido")
 
 ###################################################################
 keyboard.add_hotkey('5', Hide)
-keyboard.add_hotkey('1', InsertaMoneda)
-keyboard.add_hotkey('2', InsertaMoneda)
-keyboard.add_hotkey('enter', InsertaMoneda)
 keyboard.add_hotkey('esc', Cerrar)
+keyboard.add_hotkey('2', InsertaMoneda)
+keyboard.add_hotkey('1', InsertaMoneda)
 Bloquear()
 
 while True:
     restante = (time_start + (cont_monedas * minutos_moneda * 60)) - time.time()
     time.sleep(0.9)
     if int(restante) % 10 == 0 and int(restante) <= 60 and int(restante) > 0:
-        InsertaMoneda()
+        Hablar("Te quedan " + str(int(restante)) + " segundos")
     if insertada:
         if time.time() > time_start + (cont_monedas * minutos_moneda * 60):
             insertada = False
             Bloquear()
             cont_monedas = 0
         else:
-            print(restante)
+            print(int(restante))
     else:
         continue
---
+++ ejecutar.cmd
@@ -0,0 +1,1 @@
+"C:\Program Files (x86)\Loquendo\LTTS7\bin\TTSFileGenerator.exe" -v Jorge -q -o audio "texto.txt"
